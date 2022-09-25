d7b0990d1c952e34e01d350d58f5ac47dbdb2226
Nick Cheatwood <nick.cheatwood@gmail.com>
2022-09-24 23:58:54 -0500

    ci: shared ui

---
+++ packages/shared-ui/Button.tsx
@@ -0,0 +1,10 @@
+import React, { MouseEventHandler } from "react"
+
+interface IProps {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode
+}
+
+export function Button({ onClick, children}: IProps) {
+    return (<button onClick={onClick}>{children}</button>)
+}
